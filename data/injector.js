9699d96f150fc869f67eaaf381b0dfca27e05bb5
canuckistani <jeff@canuckistani.ca>
2014-05-08 16:30:55 -0700

    bgrins' .bind trick

--- data/injector.js
+++ data/injector.js
@@ -1,12 +1,5 @@
-function burn(text) {
-  var fireStyle = 'text-align: center;margin: 5px auto;font-family: Courier;font-size: 18px; text-transform: uppercase;color: #fff;text-shadow: 0 0 20px #fefcc9, 10px -10px 30px #feec85, -20px -20px 40px #ffae34, 20px -40px 50px #ec760c, -20px -60px 60px #cd4606, 0 -80px 70px #973716, 10px -90px 80px #451b0e;}';
-  var newArgs = ["%c"+text, fireStyle];
+// massively simpler implementation via bgrins
+var fireStyle = 'text-align: center;margin: 5px auto;font-family: Courier;font-size: 18px; text-transform: uppercase;color: #fff;text-shadow: 0 0 20px #fefcc9, 10px -10px 30px #feec85, -20px -20px 40px #ffae34, 20px -40px 50px #ec760c, -20px -60px 60px #cd4606, 0 -80px 70px #973716, 10px -90px 80px #451b0e;}';
 
-  var args = [].slice.call(arguments);
-  if (args.length > 1) {
-    newArgs = newArgs.concat(args.slice(1));
-  }
-  unsafeWindow.console.log.apply(this, newArgs);
-}
+unsafeWindow.console.burn = unsafeWindow.console.log.bind(unsafeWindow.console, "%c", fireStyle);
 
-unsafeWindow.console.burn = burn;
